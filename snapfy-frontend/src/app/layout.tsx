75c30b7e940c6a1409a035f8c47e3ad8b9ec1687
yoghantara08 <gyoghantara@gmail.com>
2025-03-12 14:08:22 +0800

    feat: button

--- snapfy-frontend/src/app/layout.tsx
+++ snapfy-frontend/src/app/layout.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Work_Sans } from "next/font/google";
 
 import LayoutWrapper from "@/components/Layout/LayoutWrapper";
 import { Providers } from "@/components/Providers/Providers";
 
 import "../styles/globals.css";
 
-const inter = Inter({
-  variable: "--font-inter",
+const worksans = Work_Sans({
+  variable: "--font-worksans",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} text-text-primary antialiased`}>
+      <body className={`${worksans.variable} antialiased`}>
         <Providers>
           <LayoutWrapper>{children}</LayoutWrapper>
         </Providers>
---
+++ snapfy-frontend/src/components/Button/Button.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import classNames from "classnames";
+
+export interface ButtonProps {
+  id?: string;
+  children: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
+  // eslint-disable-next-line no-unused-vars
+  onClick?: (() => void) | ((e: any) => void);
+  variant?: "primary" | "secondary";
+  type?: "button" | "submit" | "reset";
+}
+
+const Button = ({
+  id,
+  children,
+  className,
+  onClick,
+  disabled = false,
+  variant = "primary",
+  type = "button",
+}: ButtonProps) => {
+  return (
+    <button
+      id={id}
+      type={type}
+      onClick={!disabled ? onClick : undefined}
+      disabled={disabled}
+      className={classNames(
+        "inline-flex items-center justify-center rounded-sm px-5 py-2.5 md:py-3",
+        "text-sm font-medium transition-colors focus:outline-none md:text-base",
+        {
+          // Primary variant
+          "bg-accent-pink/20 text-accent-pink hover:bg-accent-pink/30 cursor-pointer":
+            variant === "primary" && !disabled,
+          "bg-accent-pink/20 cursor-not-allowed text-white":
+            variant === "primary" && disabled,
+
+          // Secondary variant
+          "bg-background hover:bg-surface hover:!border-surface cursor-pointer border":
+            variant === "secondary" && !disabled,
+          "bg-surface text-muted cursor-not-allowed":
+            variant === "secondary" && disabled,
+        },
+        className,
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
