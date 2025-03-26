b010a50b6f26e086a4512dd8b9c0fda82b9e2c74
yoghantara08 <gyoghantara@gmail.com>
2025-03-26 21:16:17 +0800

    implement dashboard liquidity position

--- snapfy-frontend/src/features/dashboard/components/Networth/Networth.tsx
+++ snapfy-frontend/src/features/dashboard/components/Networth/Networth.tsx
@@ -1,34 +1,49 @@
 "use client";
 import React from "react";
 
-import useUniswapTVLAndVolume from "../../hooks/useUniswapTVLAndVolume";
+import BigNumber from "bignumber.js";
+import { Address } from "viem";
+import { useAccount } from "wagmi";
+
+import { UNISWAP_V2_POOL_IDS } from "@/constant";
+import useLiquidityPosition from "@/hooks/useLiquidityPosition";
+import { useUniswapV2GetPoolById } from "@/hooks/useUniswapV2Pools";
+
+const pairAddress = UNISWAP_V2_POOL_IDS.ETH_USDC as Address;
 
 const TVLAndVolume = () => {
-  // const { data, isLoading, isError, error } = useUniswapTVLAndVolume();
-  const { isError, error } = useUniswapTVLAndVolume();
+  const { address } = useAccount();
+  const { data: position, isLoading } = useLiquidityPosition(
+    address as Address,
+    pairAddress,
+  );
+  const { data: pairData, isLoading: loading } =
+    useUniswapV2GetPoolById(pairAddress);
+  const token0share = position?.userToken0
+    .div(10 ** 18)
+    .toFixed(6)
+    .toString();
+  const token1share = position?.userToken1
+    .div(10 ** 6)
+    .toFixed(2)
+    .toString();
 
-  // const loading = isLoading || !data;
+  const currentUSDPosition = BigNumber(token0share || 0)
+    .times(pairData?.token1Price || 0)
+    .plus(token1share || 0)
+    .toFixed(2)
+    .toString();
 
-  const INFOS = [
-    {
-      title: "Networth",
-      value: "$121.22K",
-    },
-  ];
-
-  if (isError) return <div>Error: {error.message}</div>;
+  const loaded = !isLoading && !loading;
 
   return (
     <div className="flex gap-3">
-      {INFOS.map((info) => (
-        <div
-          key={info.title}
-          className="bg-accent-blue/20 text-accent-blue flex flex-col gap-1 rounded-sm p-3 pr-6 md:p-4"
-        >
-          <h4 className="text-sm text-nowrap md:text-base">{info.title}</h4>
-          <h2 className="text-xl font-semibold md:text-2xl">{info.value}</h2>
-        </div>
-      ))}
+      <div className="bg-accent-blue/20 text-accent-blue flex flex-col gap-1 rounded-sm p-3 pr-6 md:p-4">
+        <h4 className="text-sm text-nowrap md:text-base">Networth</h4>
+        <h2 className="text-xl font-semibold md:text-2xl">
+          ${loaded ? currentUSDPosition : 0}
+        </h2>
+      </div>
     </div>
   );
 };
--- snapfy-frontend/src/features/dashboard/components/Positions/Positions.tsx
+++ snapfy-frontend/src/features/dashboard/components/Positions/Positions.tsx
@@ -1,17 +1,60 @@
+"use client";
 import React, { useState } from "react";
 import { FaMinus } from "react-icons/fa6";
 
 import Image from "next/image";
 
+import BigNumber from "bignumber.js";
+import { Address } from "viem";
+import { useAccount } from "wagmi";
+
 import Button from "@/components/Button/Button";
+import { UNISWAP_V2_POOL_IDS } from "@/constant";
+import useLiquidityPosition from "@/hooks/useLiquidityPosition";
+import { useUniswapV2GetPoolById } from "@/hooks/useUniswapV2Pools";
+import { calculateAPRV2 } from "@/lib/utils/calculateAPR";
 
 import EtherIcon from "../../../../../public/Image/etherIcon.svg";
 import UsdcIcon from "../../../../../public/Image/usdcIcon.svg";
 
 import RemoveLiquidityModal from "./RemoveLiquidityModal";
 
+const pairAddress = UNISWAP_V2_POOL_IDS.ETH_USDC as Address;
+
 const Positions = () => {
+  const { address } = useAccount();
+  const { data: position, isLoading } = useLiquidityPosition(
+    address as Address,
+    pairAddress,
+  );
+  const { data: pairData, isLoading: loading } =
+    useUniswapV2GetPoolById(pairAddress);
+
   const [open, setOpen] = useState(false);
+
+  const token0share = position?.userToken0
+    .div(10 ** 18)
+    .toFixed(6)
+    .toString();
+  const token1share = position?.userToken1
+    .div(10 ** 6)
+    .toFixed(2)
+    .toString();
+
+  const currentUSDPosition = BigNumber(token0share || 0)
+    .times(pairData?.token1Price || 0)
+    .plus(token1share || 0)
+    .toFixed(2)
+    .toString();
+
+  const shareOfPool = position?.userShare.toFixed(6).toString();
+
+  const APR = calculateAPRV2(
+    pairData?.dailyFeesUSD || 0,
+    parseFloat(pairData?.reserveUSD || "0"),
+  );
+
+  const loaded = !isLoading && !loading;
 
   return (
     <div className="grid w-full gap-3 md:grid-cols-2">
@@ -48,25 +91,25 @@ const Positions = () => {
           </div>
 
           <p className="bg-opacity-green text-accent-green rounded-sm px-2 py-1">
-            4.4% APR
+            {loaded ? APR : 0}% APR
           </p>
         </div>
         <div className="bg-opacity-blue flex w-full flex-col items-center justify-center gap-2 rounded-sm p-3">
           <div className="flex w-full items-center justify-between gap-5">
             <p>Current Position</p>
-            <p>$1.03</p>
+            <p>${loaded ? currentUSDPosition : 0}</p>
           </div>
           <div className="flex w-full items-center justify-between gap-5">
             <p>Share of pool</p>
-            <p>0.002%</p>
+            <p>{loaded ? shareOfPool : 0}%</p>
           </div>
           <div className="flex w-full items-center justify-between gap-5">
             <p>Deposited ETH</p>
-            <p>0.000005</p>
+            <p>{loaded ? token0share : 0}</p>
           </div>
           <div className="flex w-full items-center justify-between gap-5">
             <p>Deposited USDC</p>
-            <p>0.5</p>
+            <p>{loaded ? token1share : 0}</p>
           </div>
         </div>
 
